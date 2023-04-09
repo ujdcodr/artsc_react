1ea4864e7e9b5f2a136f475d9eb070a0953d59ed
Omkar Masur <44014415+oam-mit@users.noreply.github.com>
2023-04-09 02:16:09 -0700

    Added confirm friend request

--- src/views/FriendRequests.js
+++ src/views/FriendRequests.js
@@ -4,14 +4,18 @@ import { useNavigate } from "react-router-dom";
 
 const FriendRequests = () => {
 	let [friend_requests, setFriendRequests] = useState([]);
+    let [friends,setFriends] = useState([]);
+    let [username,setUsername] = useState("");
+
 	let navigate = useNavigate();
 
 	useEffect(() => {
 		fetch("/social/api/get_network/")
 			.then((data) => data.json())
 			.then((data) => {
-              
 				setFriendRequests(data.friend_requests_received);
+                setFriends(data.friends);
+                setUsername(data.your_username);
 			})
 			.catch((err) => alert(err));
 	}, []);
@@ -88,6 +92,51 @@ const FriendRequests = () => {
 									<br></br>
 								</Col>
 							);
+						})}
+					</Row>
+
+                    <Row md={3}>
+						<h3 className="text-white">Your Friends</h3>
+						{friends.map((friend_request) => {
+                            if(friend_request.user2.username === username)
+                            {    return (
+                                    <Col key={friend_request.id}>
+                                        <Card
+                                            style={{ cursor: "pointer" }}
+                                        >
+                                            {friend_request.user1.first_name}
+                                            <Image src={friend_request.user1.profile_photo} thumbnail />
+                                            <button
+                                                  onClick={()=>navigate(`/social/user/${friend_request.user1.username}`)}
+                                                type="button"
+                                                className="btn btn-secondary"
+                                            >
+                                               Go to profile
+                                            </button>
+                                        </Card>
+                                        <br></br>
+                                    </Col>
+                                );}
+                            else {
+                                return (
+                                    <Col key={friend_request.id}>
+                                        <Card
+                                            style={{ cursor: "pointer" }}
+                                        >
+                                            {friend_request.user2.first_name} {friend_request.user2.last_name}
+                                            <Image src={friend_request.user2.profile_photo} thumbnail />
+                                            <button
+                                                onClick={()=>navigate(`/social/user/${friend_request.user2.username}`)}
+                                                type="button"
+                                                className="btn btn-secondary"
+                                            >
+                                                Go to profile
+                                            </button>
+                                        </Card>
+                                        <br></br>
+                                    </Col>
+                                );
+                            }
 						})}
 					</Row>
 				</div>
